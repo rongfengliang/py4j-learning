ff7da21b154848d20c4c8959d079f1ea437e6d68
rong fengliang <1141591465@qq.com>
2022-07-30 20:01:36 +0800

    add more

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from py4j.java_gateway import JavaGateway, GatewayParameters
 gateway = JavaGateway(gateway_parameters=GatewayParameters(port=11111))
 
 stack = gateway.entry_point.getStack()
-
+random = gateway.jvm.java.util.Random()
+value = random.nextInt(10)
 stack.push("First %s" % ('item'))
 
 item = stack.pop()
 
 print(item)
+print(value)
